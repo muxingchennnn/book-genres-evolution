f2a77b35407cd901a4b8dac4a33eaa0f14f83a95
Muxing Chen <ike.chan@outlook.com>
2025-02-18 11:15:57 -0500

    swarm chart prototype

---
+++ src/routes/+page.js
@@ -0,0 +1,12 @@
+// import { json } from 'd3';
+
+// console.log(json);
+
+// /** @type {import('./$types').PageLoad} */
+// export async function load() {
+// 	const data = await json('../lib/data/happiness-score.js');
+
+// 	return { data };
+// }
+
+// export const ssr = false;
